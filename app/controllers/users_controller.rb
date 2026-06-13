6d3ce601bd3a9f57dcc375846912425b91ead20a
AmrShoukry <shoukryworkamr1@gmail.com>
2026-06-13 14:06:24 +0300

    Added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number], address: params[:user][:address])
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /users/:id/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number], address: params[:user][:address])
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
+  end
+end
