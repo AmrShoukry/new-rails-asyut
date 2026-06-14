c5713092c2777c4645b57b4fa12fb1b203ec6e75
AmrShoukry <shoukryworkamr1@gmail.com>
2026-06-14 13:45:46 +0300

    new Many-to-Many association PostEditor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
+
+  has_many :editings
+  has_many :editors, through: :editings, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+
+  has_many :editings
+  has_many :edited_posts, through: :editings, source: :post
 end
